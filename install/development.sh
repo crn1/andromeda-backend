32f1b7611d7f4e96feac675a3b7757a77e5686bb
crn1 <gluvajic@aol.com>
2019-10-03 11:24:51 +0200

    Fixed commit errors

--- install/development.sh
+++ install/development.sh
@@ -4,13 +4,13 @@ python3 -m venv venv
 
 echo "" >> ./venv/bin/activate
 echo "export FLASK_ENV=development" >> ./venv/bin/activate
-echo "export FLASK_APP=andromeda-backend:app" >> ./venv/bin/activate
+echo "export FLASK_APP=application:app" >> ./venv/bin/activate
 
 echo "" >> ./venv/bin/activate.fish
 echo "export FLASK_ENV=development" >> ./venv/bin/activate.fish
-echo "export FLASK_APP=andromeda-backend:app" >> ./venv/bin/activate.fish
+echo "export FLASK_APP=application:app" >> ./venv/bin/activate.fish
 
-python3 -c 'import os; print("secret_key = ", str(os.urandom(32)))' >> ./andromeda-backend/secrets.py
+python3 -c 'import os; print("secret_key = ", str(os.urandom(32)))' >> ./application/secrets.py
 
 . ./venv/bin/activate
 pip install -r requirements.txt
